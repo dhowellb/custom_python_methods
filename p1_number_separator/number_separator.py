08c08be403c54cc664aa8ea7e806df7007564689
Duke Buerano <dukebueranoo@gmail.com>
2026-04-27 18:35:55 +0800

    step 3: implement file writing logic with loop to generate and save random integers

--- p1_number_separator/number_separator.py
+++ p1_number_separator/number_separator.py
@@ -9,3 +9,7 @@ class NumberSeparator:
     def generate_dummy_data(self):
         if not os.path.exists(self.input_file_name):
             print("Generating input file with random integers...")
+            with open(self.input_file_name, "w") as target_file_object:
+                for i in range(20):
+                    random_integer = random.randint(1, 100)
+                    target_file_object.write(f"{random_integer}\n")
