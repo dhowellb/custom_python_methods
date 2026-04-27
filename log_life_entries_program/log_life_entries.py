761ced0107a37207085c8c3552693431f3ad54b1
Duke Buerano <dukebueranoo@gmail.com>
2026-04-27 20:21:57 +0800

    ginawa ang write entries method at nag-set ng context manager para sa file writing operations

--- log_life_entries_program/log_life_entries.py
+++ log_life_entries_program/log_life_entries.py
@@ -2,3 +2,6 @@ class LifeLogger:
 
     def __init__(self, output_file_name="mylife.txt"):
         self.output_file_name = output_file_name
+    def write_entries(self):
+        with open(self.output_file_name, "w") as output_file_object:
+            pass
