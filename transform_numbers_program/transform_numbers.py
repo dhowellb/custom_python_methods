dbe229d6c40e18b71cdae81de26ad7343c5ea030
Duke Buerano <dukebueranoo@gmail.com>
2026-04-27 21:18:00 +0800

    kinumpleto ang generator para gumawa ng 20 random integers at isulat sa text file

--- transform_numbers_program/transform_numbers.py
+++ transform_numbers_program/transform_numbers.py
@@ -10,3 +10,7 @@ class NumberTransformer:
     def generate_dummy_data(self):
         if not os.path.exists(self.input_file_name):
             print("Generating integers file...")
+            with open(self.input_file_name, "w") as target_file_object:
+                for i in range(20):
+                    random_integer = random.randint(1, 20)
+                    target_file_object.write(f"{random_integer}\n")
